af0c015a194f5869298d8a4e27902929118edcd9
barnettx <barnett.lee.zhi.le@gmail.com>
2016-10-16 09:00:20 +0800

    added sabre stuff remember to npm install sabre-dev-studio

--- server/index.js
+++ server/index.js
@@ -15,6 +15,13 @@ var ctrlPois = require('./controllers/poi.controllers.js');
 var ctrlCountries = require('./controllers/countries.controllers.js');
 var ctrlTrips = require('./controllers/trips.controllers.js');
 
+var SabreDevStudio = require('sabre-dev-studio');
+var sabre_dev_studio = new SabreDevStudio({
+  client_id:     'V1:6cqvyh90cnm7ftvg:DEVCENTER:EXT',
+  client_secret: '4wmBAP1l',
+  uri:           'https://api.test.sabre.com'
+});
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.send("hello");
@@ -44,6 +51,23 @@ router
   .route('/trips')
   .get(ctrlTrips.pastTripsGetAll)
   .post(ctrlTrips.pastTripsAddOne);
+  
+ router.get('/hello',function (req, res, next) {
+    sabre_dev_studio.get('/v2/shop/flights/fares?origin='+ 
+      req.query.origin +'&lengthofstay=3%2C4%2C5&theme='+
+      req.query.theme+'&maxfare='+
+      req.query.maxFare+'&pointofsalecountry='+
+      req.query.pointOfSaleCountry, {}, function(error, data) {
+    if (error) {
+      res.send(error);
+    } else {
+      var test = JSON.parse(data);
+      
+      res.send(test.FareInfo.slice(0,5));
+    }
+  });
+  
+});
 
 
 module.exports = router;
