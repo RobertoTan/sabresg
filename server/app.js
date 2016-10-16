b0c57e20d20fb4eb94fdd87500c2a3160d7bd6c3
barnettx <barnett.lee.zhi.le@gmail.com>
2016-10-16 08:14:34 +0800

    server's side not working?

--- server/app.js
+++ server/app.js
@@ -10,8 +10,6 @@ var mongoose = require('mongoose');
 
 // require('./config/passport');
 
-mongoose.connect('mongodb://zhak:zhak@ds011429.mlab.com:11429/zhak');
-
 var app = express();
 
 var routes = require('./index.js');
@@ -21,6 +19,8 @@ require('./models/poi.models.js');
 require('./models/country.models.js');
 require('./models/hotels.models.js');
 require('./models/trips.models.js');
+
+mongoose.connect('mongodb://zhak:zhak@ds011429.mlab.com:11429/zhak');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
